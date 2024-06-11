30660f12d442541e476aa99f2e1d77e1d4a7c2f5
santhosh-mrav <santhoshmodi051@gmail.com>
2024-06-11 19:12:52 +0530

    adding few console statements

--- bankManagementSystem/src/BankAccount.java
+++ bankManagementSystem/src/BankAccount.java
@@ -22,11 +22,9 @@ public class BankAccount {
 
     void PreviousTransaction(){
         if (previosTransaction>0){
-            System.out.println("deposited:");
-            System.out.println(previosTransaction);
+            System.out.println("Credited:"+previosTransaction);
         } else if (previosTransaction<0) {
-            System.out.println("withdraw:");
-            System.out.println(Math.abs(previosTransaction));
+            System.out.println("Debited:"+Math.abs(previosTransaction));
         }
         else {
             System.out.println("No transction is found.");
@@ -54,16 +52,17 @@ public class BankAccount {
                     System.out.println("Enter the amount");
                     amount = in.nextInt();
                     deposit(amount);
-                    System.out.println( " Bank Balance: "+balance);
+                    System.out.println(amount+"/- rupees Credited in your account.");
                     break;
                 case 'b':
                     System.out.println("Enter the amount");
                     amount = in.nextInt();
                     withdraw(amount);
+                    System.out.println(amount+ "/- rupees Debited from your account.");
                     break;
                 case 'c':
                     System.out.println("Balance");
-                    System.out.println(balance);
+                    System.out.println(balance+"/-");
                     break;
                 case 'd':
                     PreviousTransaction();
