e220f0abb664adce4a75bfc82d5d3bf35c8aeaa5
santhosh-mrav <santhoshmodi051@gmail.com>
2024-06-11 16:55:58 +0530

    details

--- bankManagementSystem/src/Main.java
+++ bankManagementSystem/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello guru namasthey!");
-
+        System.out.println("hello");
         System.out.println("My name is santhosh and my father name is Mallikarjuna");
     }
 }
